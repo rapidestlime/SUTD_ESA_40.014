91a77a110c9168fc6e66bb38ebdcad16767cc55e
Nicholas Tan Yi Da <rapidestlime@gmail.com>
2021-03-21 12:02:00 +0800

    added more funcs

--- login_registration.R
+++ login_registration.R
@@ -41,3 +41,85 @@ loginModal <- function(failed = FALSE) {
     )
   )
 }
+
+
+getAWSConnection <- function(){
+  conn <- dbConnect(
+    drv = RMySQL::MySQL(),
+    dbname = "student063",
+    host = "esddbinstance-1.ceo4ehzjeeg0.ap-southeast-1.rds.amazonaws.com",
+    username = "student063",
+    password = getOption("AWSPassword"))
+  conn
+}
+
+
+getPlayerID <- function(playername,password){
+  #open the connection
+  conn <- getAWSConnection()
+  #password could contain an SQL insertion attack
+  #Create a template for the query with placeholders for playername and password
+  querytemplate <- "SELECT * FROM LeaderPlayer WHERE playername=?id1 AND password=?id2;"
+  query<- sqlInterpolate(conn, querytemplate,id1=playername,id2=password)
+  result <- dbGetQuery(conn,query)
+  # If the query is successful, result should be a dataframe with one row
+  if (nrow(result)==1){
+    playerid <- result$playerid[1]
+  } else {
+    playerid <- 0
+  }
+  #Close the connection
+  dbDisconnect(conn)
+  # return the playerid
+  playerid
+}
+
+getRandomPlayerName <- function(conn){
+  #Given a connection, call the View 'LeaderRandomName' and return the resulting name
+  result <- dbGetQuery(conn,"SELECT * FROM LeaderRandomName")
+  # result should be a dataframe with a single row and a column named 'playername'
+  playername <- result$playername[1]
+  # To test what happens when there is a duplicate entry, we can override the random result
+  #playername <- "SophisticatedImaginaryZoo" # This matches an existing player in my database
+  playername
+}
+
+createNewPlayerQuery <- function(conn,playername,password){
+  #password could contain an SQL insertion attack
+  #Create a template for the query with placeholder for  password
+  querytemplate <- "INSERT INTO LeaderPlayer (playername,password) VALUES (?id1,?id2);"
+  query<- sqlInterpolate(conn, querytemplate,id1=playername,id2=password)
+}
+
+registerPlayer <- function(password){
+  #open the connection
+  conn <- getAWSConnection()
+  playername <- getRandomPlayerName(conn)
+  query <- createNewPlayerQuery(conn,playername,password)
+  # This query could fail to run properly so we wrap it in a loop with tryCatch()
+  success <- FALSE
+  iter <- 0
+  MAXITER <- 5
+  while(!success & iter < MAXITER){
+    iter <- iter+1
+    tryCatch(
+      {  # This is not a SELECT query so we use dbExecute
+        result <- dbExecute(conn,query)
+        success <- TRUE
+      }, error=function(cond){print("registerPlayer: ERROR")
+        print(cond)
+        # The query failed, likely because of a duplicate playername
+        playername <- getRandomPlayerName(conn)
+        query <- createNewPlayerQuery(conn,playername,password) }, 
+      warning=function(cond){print("registerPlayer: WARNING")
+        print(cond)},
+      finally = {print(paste0("Iteration ",iter," done."))
+      }
+    )
+  } # end while loop
+  # This may not have been successful
+  if (!success) playername = NULL
+  #Close the connection
+  dbDisconnect(conn)
+  playername
+}
